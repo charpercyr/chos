1471b7d5cdf618a773638a04fe95eff646edb0de
Christian Harper-Cyr <charpercyr@gmail.com>
2021-02-17 10:10:24 -0800

    Removed tests

--- boot/src/main.rs
+++ boot/src/main.rs
@@ -5,10 +5,4 @@
 #![feature(const_fn_transmute)]
 #![feature(global_asm)]
 
-#![feature(custom_test_frameworks)]
-#![test_runner(crate::tests::test_runner)]
-#![reexport_test_harness_main = "test_main"]
-
 mod arch;
-
-// extern crate alloc;
